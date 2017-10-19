6add2b972bfd65357c8ae037e809b6886a69e853
Yash Rane <yash_rane@umail.ucsb.edu>
2017-10-18 19:48:24 -0700

    Parsed introduction strigs for more data and improved error handling.

--- DiscordScraper.py
+++ DiscordScraper.py
@@ -1,99 +1,89 @@
 import discord
 import asyncio
 import csv
+import re
 
 client = discord.Client()
 
-authorized_users = ['insert user ids here']
 
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+admin_ids = ['225119587392421888']	#currently, it only works with one admin id.
+									#It will be changed in the future to support messaging multiple people
 	
+
+#parses an introduction message
+#returns a tuple with scraped info
+def parseIntroduction(introduction):
+	clean_intro = removeTrailingWhitespace(introduction)
+	pattern = re.compile(r'2.(.+)\/(.+)\/(.+)\s3.(.+)\s4.(.+)')
+	result = pattern.search(clean_intro)
+	if result is not None:
+		return result.groups()
+
+#helper function for parseIntroduction
+#removes any consecutive whitespace characters and replaces them with a new line		
+def removeTrailingWhitespace(text):
+	pattern = re.compile(r'([ ]+)\s')
+	return str(pattern.sub('\n', text))
 	
-#All data should be anonymous: Never store the user id or nickname
-#TODO: csv file with message information 
-#TODO: csv file for user information(roles, time of joining, introduction info)
-#TODO: check to see if we can get old archived data automatically using the bot
+#logs a message from all channels except those in the blacklist
+def logMessage(message, introduction):
 
-def get_logs():
-	NUM_LIMIT = 1000
-	CHANNEL_ID = "insert channel id here"
-	SERVER_ID = "insert server id here"
-	print(client.servers[0])
-	
-	#client.logs_from()
-
-
-
-
-
-
-
-
-@client.event
-async def on_message(message):
-	if message.content.startswith('!test'):
-		get_logs()
-		counter = 0
-		tmp = await client.send_message(message.channel, 'Calculating messages...')
-		async for log in client.logs_from(message.channel, limit=100):
-			if log.author == message.author:
-				counter += 1
-
-				await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-				
-	elif message.content.startswith('!stop') and message.author.id in authorized_users:
-		await client.logout()
-		csvfile.close()
+	#logs data to introduction.csv if it comes from the introduction channel
+	if message.channel.id == introduction:
+		intro_data = parseIntroduction(message.clean_content)
+		if intro_data is not None:
+			print(intro_data)
+			intro_file.writerow(intro_data)
+			
+	#logs data from all other channels to messages.csv
 	else:
-		roles = [r.name for r in message.author.roles[1:]]
-		
+		roles = [r.name for r in message.author.roles[1:]]		
 		messageInfo = (roles, str(message.timestamp), message.channel.name, message.clean_content )#NOTE: @here and @everyone mentions include the unicode zero width space for whatever reason
 		print (messageInfo)
 		csvwriter.writerow(messageInfo)
+
+#messages people in the admin_ids list with the specified message, then closes the client	
+async def messageAdmins(message):
+	admins = []
+	for id in admin_ids:
+		a = await client.get_user_info(id)
+		admins.append(a)
 		
-csvfile = open("messages.csv", 'a')
-csvwriter = csv.writer(csvfile)
+	for admin in admins:
+		await client.send_message(admin, message)	
+
+		
+#on startup
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+
+#when a message is sent or received
+@client.event
+async def on_message(message):
+
+	try:
+		if type(message.author) == discord.Member:
+			logMessage(message, '368219345806950400')
+	except:
+		print('There was an error somewhere.\n Message id: ' + message.id)
+		await messageAdmins('There was an error somewhere.\n Message id: ' + message.id)
+	
+		
+		
+
+#opens files to be written to
+intro_file = open('introductions.csv', 'a')
+intro_writer = csv.writer(intro_file)
+print('opened introductions.csv to append')
+
+message_file = open("messages.csv", 'a')
+csvwriter = csv.writer(message_file)
 print("Opened messages.csv to append")
  
-client.run('insert bot id here')		
-		
-		
-		
-		
-# Sample Introductions:
+client.run('MzY3NDA0MzI2MTg1OTkyMTkz.DL8TNg.4wwh6qNd-poiMkLHNEM0uxd065A')		
 
 
-# Oppen_heimer - 07/22/2017
-# 1)Oppen_heimer#0725
-# 2) UCSB/2nd/CS
-# 3) idk, ask guy fieri
-# 4) yashtritch?
-# 5) never bringing the bot back <---- you sure about that?
-
-# HuntDucks - 07/26/2017
-# huntducks #3238
-# ucsb/2017/cs
-# was in server before
-# reddit
-
-		#all on seperate lines
-# DJ Megatron - 09/18/2017
-# 1) DJ Megatron #0596
-# 2) UCSB 3rd year Biology
-# 3) I'm with koolkbraza
-# 4) reddit moderators told me to come		
-		
-
-#NOTE: we only need messages from July 18, 2017 and onwards		
-
-#introduction is a string which should contain a single introduction
-#multiline introductions should be combined before being put into this function
-def parseIntroduction(introduction):	
-
-	return ""
