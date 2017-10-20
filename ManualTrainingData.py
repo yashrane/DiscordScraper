25774687592f7eddc7e64ec20f45e4882cf9684e
Brian Lim <brian01.lim@gmail.com>
2017-10-20 03:40:57 +0000

    Added console client to manual classify training data for toxicity determination

---
+++ ManualTrainingData.py
@@ -0,0 +1,26 @@
+import csv
+import sys
+
+save_line = open('line.txt', 'r')
+line = int(save_line.readline())
+save_line.close()
+save_line = open('line.txt', 'w')
+count = 0
+toxicTrain = open('toxicTrain.csv', 'a')
+csvToxicTrain = csv.writer(toxicTrain)
+with open('messages.csv') as messages:
+    csvMessages = csv.reader(messages)
+    for row in csvMessages:
+        print(row[3])
+        if(count >= line):
+            Toxicity = input('t for toxic, n for not toxic, s for skip, q to quit: ')
+            if Toxicity == 'q':
+                save_line.write(str(count))
+                print('Saving at line ' + str(count))
+                sys.exit()
+            elif Toxicity == 't':
+                csvToxicTrain.writerow((row[3], 't'))
+            elif Toxicity == 'n':
+                csvToxicTrain.writerow((row[3], 'n'))
+        count = count + 1
+
